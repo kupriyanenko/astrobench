06ce38c0b6ad5ca046234cd3a21ec9dbfc6761d9
kupriyanenko <a.kupriyanenko@gmail.com>
2014-05-03 17:45:32 +0300

    update case with non setup suite

--- dist/main.js
+++ dist/main.js
@@ -7664,7 +7664,7 @@ __p+='<div class="suite" id="'+
 '">\n\t<div class="suite-title fn-show-setup">\n\t\t<h1 class="suite-title-text">'+
 ((__t=( suite.suite.name ))==null?'':__t)+
 '</h1>\n\t\t<span class="suite-controls">\n\t\t\t<a href="#" class="fn-run-suite">Run suite</a>\n\t\t</span>\n\t</div>\n\t<div class="suite-setup hidden">\n\t\t<pre><code>'+
-((__t=( hilite(fnstrip(suite.setupFn)) ))==null?'':__t)+
+((__t=( suite.setupFn && hilite(fnstrip(suite.setupFn)) ))==null?'':__t)+
 '</code></pre>\n\t</div>\n\t<div class="fn-benchs suite-bench-list"></div>\n</div>';
 }
 return __p;
--- examples/bench.js
+++ examples/bench.js
@@ -15,12 +15,6 @@ describe("A suite", function() {
 });
 
 describe("B suite", function() {
-  var one;
-
-  setup(function(suite) {
-    one = 100;
-  });
-
   bench("String#match", function() {
     !! "Hello world".match(/o/);
   });
