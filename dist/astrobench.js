31cf01aa2b05c38592890a094c753afef2f3f7a1
kupriyanenko <a.kupriyanenko@gmail.com>
2014-06-22 19:25:46 +0300

    replace grunt by gulp, rename dist file, add min

--- dist/astrobench.js
+++ dist/astrobench.js
@@ -1,12 +1,12 @@
 /*!
- * Astrobench v0.0.3 - 2014-05-23 - Library for JavaScript benchmarks based on Benchmark.js
+ * Astrobench - 0.1.0
+ * Library for JavaScript benchmarks based on Benchmark.js
  *
  * https://github.com/kupriyanenko/astrobench
  *
- * Copyright 2014 Alexey Kupriyanenko <a.kupriyanenko@gmail.com>
+ * Copyright Alexey Kupriyanenko <a.kupriyanenko@gmail.com>
  * Released under the MIT license.
  */
-
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
  * jBone v1.0.16 - 2014-05-07 - Library for DOM manipulation
@@ -8008,7 +8008,7 @@ exports.drawBench = function(suite, bench) {
         });
 };
 
-},{"./astrobench":3,"./templates/app.html":4,"./templates/bench.html":5,"./templates/suite.html":6,"jbone":1,"lodash":2}]},{},[3,7])
+},{"./astrobench":3,"./templates/app.html":4,"./templates/bench.html":5,"./templates/suite.html":6,"jbone":1,"lodash":2}]},{},[7])
 /*!
  * Benchmark.js v1.0.0 <http://benchmarkjs.com/>
  * Copyright 2010-2012 Mathias Bynens <http://mths.be/>
